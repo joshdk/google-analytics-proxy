d88c6a90d532f4da3a337aad9dfeedab58a7b63f
Josh Komoroske <jdkomo@gmail.com>
2021-08-21 10:20:48 -0400

    chore(go): basic go mod and main

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/joshdk/google-analytics-proxy
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,13 @@
+// Copyright Josh Komoroske. All rights reserved.
+// Use of this source code is governed by the MIT license,
+// a copy of which can be found in the LICENSE.txt file.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("google-analytics-proxy")
+}
