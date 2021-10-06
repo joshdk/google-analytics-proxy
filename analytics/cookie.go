aaadb1eb5cca8ebad5c7fe24368acf1ddc3659fc
Josh Komoroske <jdkomo@gmail.com>
2021-10-05 20:54:08 -0700

    docs: extract utm header names into constants and document

--- analytics/cookie.go
+++ analytics/cookie.go
@@ -11,18 +11,22 @@ import (
 	"github.com/gofrs/uuid"
 )
 
+// Cookie is a client cookie that is used to pseudonymously identify a
+// particular user across multiple requests.
+const Cookie = "_gap"
+
 const twoYears = time.Hour * 24 * 365 * 2
 
-func getCookie(request *http.Request, name string) (string, *http.Cookie) {
+func getCookie(request *http.Request) (string, *http.Cookie) {
 	// Check if the named cookie was provided along with the request and return
 	// the value it contains.
-	if cookie, _ := request.Cookie(name); cookie != nil {
+	if cookie, _ := request.Cookie(Cookie); cookie != nil {
 		return cookie.Value, nil
 	}
 
 	// The named cookie was not provided, so make a new one.
 	cookie := &http.Cookie{
-		Name: name,
+		Name: Cookie,
 
 		// A UUID (version 4) for identifying the current client.
 		// See: https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters#cid
