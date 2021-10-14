9bc2fb7fb11bae0195667d712078b857d1ad6788
Josh Komoroske <jdkomo@gmail.com>
2021-10-13 23:50:06 -0700

    docs: add spdx license identifier comments (#42)

--- analytics/tracker.go
+++ analytics/tracker.go
@@ -1,6 +1,7 @@
 // Copyright Josh Komoroske. All rights reserved.
 // Use of this source code is governed by the MIT license,
 // a copy of which can be found in the LICENSE.txt file.
+// SPDX-License-Identifier: MIT
 
 // Package analytics provides functionality for interacting with the Google
 // Analytics API.
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 // Copyright Josh Komoroske. All rights reserved.
 // Use of this source code is governed by the MIT license,
 // a copy of which can be found in the LICENSE.txt file.
+// SPDX-License-Identifier: MIT
 
 package main
 
@@ -22,7 +23,7 @@ var version = "development"
 
 func main() {
 	if err := mainCmd(); err != nil {
-		fmt.Println("joshdk/google-analytics-proxy:", err) //nolint:forbidigo
+		fmt.Println("joshdk/google-analytics-proxy:", err) // nolint:forbidigo
 		os.Exit(1)
 	}
 }
